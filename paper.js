d7510f46389fd2db517df4ed12c5dbe461437128
shivanshanand <74009335+shivanshanand@users.noreply.github.com>
2020-11-27 17:35:56 +0530

    Update paper.js

--- paper.js
+++ paper.js
@@ -6,10 +6,9 @@ class paper{
             friction: 0.5,
             density:1.2
         }
-        this.body=Bodies.circle(x,y,radius,op);
+        this.body=Bodies.circle(x,y,radius/2,op);
         this.radius= radius;
         World.add(world,this.body);
-
     }
 
     display(){
@@ -17,26 +16,7 @@ class paper{
         fill("pink"); 
         translate(this.body.position.x, this.body.position.y ); 
         rotate(this.body.angle);
-        ellipse(0,0, this.radius, this.r); 
+        ellipse(0,0, this.radius, this.radius); 
         pop();
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
